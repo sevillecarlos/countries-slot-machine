cadd88ed9053dec8ee08361d07a6224c4f6ea923
Carlos Sevilla <sevillecarlos@gmail.com>
2021-10-09 11:29:19 -0600

    fix the issue of the unique country form

--- src/components/ListCountryForm.tsx
+++ src/components/ListCountryForm.tsx
@@ -4,22 +4,19 @@ import { RootStateOrAny } from "react-redux";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { getUniqueCountry } from "../app/slices/countries";
 
-import "./style/UniqueCountryForm.css";
-
-const UniqueCountryForm = () => {
+const ListCountryForm = () => {
   const dispatch = useAppDispatch();
   const country = useAppSelector((state: RootStateOrAny) => state.countries);
 
   const [countryQuery, setCountryQuery] = useState("");
 
-  const submitUniqueCountryForm = (e: any) => {
+  const submitListCountryForm = (e: any) => {
     e.preventDefault();
-    dispatch(getUniqueCountry(countryQuery.toLowerCase()));
+   
   };
-
   return (
     <div>
-      <Form className="unique-contry-form" onSubmit={submitUniqueCountryForm}>
+      <Form className="unique-contry-form" onSubmit={submitListCountryForm}>
         <Form.Group className="mb-3" controlId="formCountryName">
           <Form.Label>Type Country Name</Form.Label>
           <Form.Control
@@ -31,22 +28,10 @@ const UniqueCountryForm = () => {
         <Button type="submit">Submit</Button>
       </Form>
       <div>
-        {country?.uniqueCountry !== null &&
-        !country?.uniqueCountry?.notFoundMsg ? (
-          <ul key={country?.uniqueCountry?.countryName}>
-            <li>
-              Country: {country?.uniqueCountry?.countryName}{" "}
-              {country?.uniqueCountry?.flag}
-            </li>
-            <li>Capital: {country?.uniqueCountry?.capital?.join(', ')}</li>
-            <li>Region: {country?.uniqueCountry?.region}</li>
-          </ul>
-        ) : (
-          <p>{country?.uniqueCountry?.notFoundMsg}</p>
-        )}
+        
       </div>
     </div>
   );
 };
 
-export default UniqueCountryForm;
+export default ListCountryForm;
