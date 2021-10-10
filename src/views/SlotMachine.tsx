4fb250aa1a3008b1a66ab041f4ea1baee8baec8b
Carlos Sevilla <sevillecarlos@gmail.com>
2021-10-10 12:05:05 -0600

    add the spin function to slot machine

--- src/views/SlotMachine.tsx
+++ src/views/SlotMachine.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import "./style/SlotMachine.css";
+import { getRandomNumber } from "../helpers/getRandomNumber";
 import { SLOTMACHINE } from "../config";
 
 const SlotMachine = () => {
+  const [reelNumber, setReelNumber] = useState({
+    reelNumber1: 0,
+    reelNumber2: 0,
+    reelNumber3: 0,
+  });
+
+  const spin = () => {
+    setReelNumber({
+      reelNumber1: getRandomNumber(0, SLOTMACHINE.reel1.length),
+      reelNumber2: getRandomNumber(0, SLOTMACHINE.reel2.length),
+      reelNumber3: getRandomNumber(0, SLOTMACHINE.reel3.length),
+    });
+  };
+
+  console.log(reelNumber)
   return (
     <div>
       <div className="slot-machine-container">
-        <span>{SLOTMACHINE.reel1[0]}</span>
-        <span>{SLOTMACHINE.reel2[0]}</span>
-        <span>{SLOTMACHINE.reel3[0]}</span>
+        <span>{SLOTMACHINE.reel1[reelNumber.reelNumber1]}</span>
+        <span>{SLOTMACHINE.reel2[reelNumber.reelNumber2]}</span>
+        <span>{SLOTMACHINE.reel3[reelNumber.reelNumber3]}</span>
       </div>
       <div className="slot-machine-btn-container">
-        <Button>Spin</Button>
+        <Button onClick={spin}>Spin</Button>
       </div>
     </div>
   );
